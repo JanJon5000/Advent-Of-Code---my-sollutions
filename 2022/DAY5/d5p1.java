bd822cc1e687eca29d3cf8a29d9bae8e0246ee9e
unknown <jan@ipoczta.pl>
2025-04-24 02:27:59 +0200

    day 5 part 1 completed

---
+++ 2022/DAY5/d5p1.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.File;
+import java.io.IOException;
+
+public class d5p1{
+    public static void main(String[] args){
+        try {
+            File data = new File("C:\\Users\\janj\\.vscode\\VSCODE\\AOC23\\2022\\DAY5\\data.txt");
+            Scanner myScanner = new Scanner(data);
+            byte n = 9;
+            String[][] arrayStacks = new String[n][8];
+            short loopRounds = 502;
+            for(byte i=0;i<8;i++){
+                String line = myScanner.nextLine();
+                for(byte j=0;j<n;j++){
+                    arrayStacks[j][i] = line.substring(4*j, 4*j+4);
+                }
+            }
+            @SuppressWarnings("unchecked")
+            Stack<String>[] stacks = new Stack[n];
+            for(byte j=0;j<n;j++){
+                stacks[j] = new Stack<>();
+                for(byte i=7;i>=0;i--){
+                    if (stacks[j] != null && arrayStacks[j][i] != null && arrayStacks[j][i].trim().isEmpty() == false) {
+                        stacks[j].push(arrayStacks[j][i]);
+                    }
+                }
+            }
+            for(short i=0;i<loopRounds;i++){
+                List<String> line = new ArrayList<>(Arrays.asList(myScanner.nextLine().split(" ")));
+                line.remove(0);
+                line.remove(1);
+                line.remove(2);
+                for(int j=0;j<Integer.parseInt(line.get(0));j++){
+                    String removed = stacks[Integer.parseInt(line.get(1))-1].pop();
+                    stacks[Integer.parseInt(line.get(2))-1].push(removed);
+                }
+            }
+            String endString = "";
+            for(byte i=0;i<9;i++){
+                String add = stacks[i].pop().replace("]", "").replace("[", "");
+                endString += add;
+            }
+            System.out.println(endString);
+            myScanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 <p> It started in 2023 when I was solving challanges in python. I couldnt solve all of them but had a lot of fun with trying. Today - 13 April 2025 i returned to it with a new idea. I will try to solve diffrent years' challanges with diffrent programming languages. Its propably not original but here is a list of what I at least started to solve and with which language and how it is going:</p>
 
 <ul>
-  <li> <a href=https://adventofcode.com/2022> Advent Of Code 2022 </a> - collected <strong>8</strong> stars with Java 23.0+ </li>
+  <li> <a href=https://adventofcode.com/2022> Advent Of Code 2022 </a> - collected <strong>9</strong> stars with Java 23.0+ </li>
   <li> <a href=https://adventofcode.com/2023> Advent Of Code 2023 </a> - collected <strong>15</strong> stars with Python 3.9+ </li>
 </ul>
